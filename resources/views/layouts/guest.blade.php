56cca99b9008b5864dfa8de40ccaef7d9d3660ea
saadsebai <saadsebaii@gmail.com>
2022-08-31 16:41:13 +0100

    vulnerabilities crud

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'Vulnerabilities-App') }}</title>
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
@@ -15,6 +15,21 @@
     </head>
     <body>
         <div class="font-sans text-gray-900 antialiased">
+
+            @if (Route::has('login'))
+                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                    @auth
+                        <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
+                    @else
+                        <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+
             {{ $slot }}
         </div>
     </body>
---
+++ resources/views/vulnerabilities/vulnerabilities-edit.blade.php
@@ -0,0 +1,86 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Vulnerabilities') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+
+                    <form action="{{ route('vulnerabilities.update', $vulnerability) }}" method="POST">
+                        @csrf
+                        @method('PUT')
+
+                        <div class="mb-6">
+                            <label for="title"
+                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                                {{ __('Title') }} * :
+                            </label>
+                            <input type="text"
+                                id="title"
+                                name="title"
+                                value="{{ old('title', $vulnerability) }}"
+                                class="bg-gray-50 border mb-5 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                placeholder="{{ __('Title') }}" required>
+
+                                @error('title')
+                                <div class="mt-2">
+                                    <span class="text-red-400">{{ $message }}</span>
+                                </div>
+                                @enderror
+                        </div>
+
+                        <div class="mb-6">
+                            <label for="description"
+                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                                {{ __('Description') }} * :
+                            </label>
+                            <textarea type="text"
+                                id="description"
+                                name="description"
+                                class="bg-gray-50 border mb-5 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                placeholder="{{ __('Description') }}"
+                                required>{{ old('description', $vulnerability) }}</textarea>
+
+                                @error('description')
+                                <div class="mt-2">
+                                    <span class="text-red-400">{{ $message }}</span>
+                                </div>
+                                @enderror
+                        </div>
+
+                        <div class="mb-6">
+                            <label for="rank"
+                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                                {{ __('Rank') }} :
+                            </label>
+                            <input type="number"
+                                id="rank"
+                                name="rank"
+                                min="1"
+                                value="{{ old('rank', $vulnerability) }}"
+                                class="bg-gray-50 border mb-5 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                placeholder="{{ __('Rank') }}" required>
+
+                                @error('rank')
+                                <div class="mt-2">
+                                    <span class="text-red-400">{{ $message }}</span>
+                                </div>
+                                @enderror
+                        </div>
+
+                        <button type="submit"
+                            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                        >
+                            Edit
+                        </button>
+                    </form>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
