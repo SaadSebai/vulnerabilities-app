4384c35def009feeb84b8b5d29eafa6105f71545
saadsebai <saadsebaii@gmail.com>
2023-01-15 12:45:43 +0100

    wip

--- resources/views/vulnerabilities/vulnerabilities-edit.blade.php
+++ resources/views/vulnerabilities/vulnerabilities-edit.blade.php
@@ -63,7 +63,7 @@
                                 min="1"
                                 value="{{ old('rank', $vulnerability) }}"
                                 class="bg-gray-50 border mb-5 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                placeholder="{{ __('Rank') }}" required>
+                                placeholder="{{ __('Rank') }}">
 
                                 @error('rank')
                                 <div class="mt-2">
